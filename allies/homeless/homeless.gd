edb20436967e4afae7d5e80e7b0b293561a68dfd
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2023-11-10 04:42:22 +0800

    added: homeless

---
+++ allies/homeless/homeless.gd
@@ -0,0 +1,80 @@
+extends CharacterBody2D
+
+class_name Homeless
+
+@onready var rng: RandomNumberGenerator = RandomNumberGenerator.new()
+@onready var idle_timer: Timer = $IdleTimer
+@onready var velocity_component: VelocityComponent = $VelocityComponent
+@onready var animation_component: AnimationComponent = $AnimationComponent
+@onready var enemydetection_component: EnemyDetectionComponent = $EnemyDetectionComponent
+@onready var max_speed = velocity_component.max_speed
+@onready var enemy: CharacterBody2D
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var direction: Vector2 = Vector2.RIGHT
+
+var idle: bool = true
+var new_direction: int = 0
+var enemy_distance: float
+var shooting_angle: float
+var run_away: bool = false
+
+var target_animal: CharacterBody2D = null
+var target_animal_distance: int = 1000
+var animals_list: Array
+
+
+func _ready():
+	idle_timer.start()
+
+func _physics_process(delta):
+	on_idle()
+	set_target_enemy()
+	
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	if idle:
+		direction.x = new_direction
+		velocity.x = direction.x * max_speed
+	elif enemy != null:
+		get_direction()
+		velocity.x = direction.x * max_speed
+	elif not idle:
+		direction.x = 0
+		velocity.x = move_toward(velocity.x, 0, max_speed)
+	
+	move_and_slide()
+	animation_component.update_animation(direction)
+	animation_component.update_facing_direction(direction)
+
+func get_direction():
+	if enemy != null:
+		direction = (enemy.global_position - global_position).normalized()
+		if enemy_distance < 120 and not run_away:
+			run_away = true
+		elif enemy_distance > 200 and run_away:
+			run_away = false
+		elif run_away:
+			direction.x = -direction.x
+
+func on_idle():
+	if enemy != null:
+		idle = false
+	elif idle_timer.is_stopped():
+		idle = true
+		idle_timer.start()
+
+func _on_idle_timer_timeout():
+	var choice: int = rng.randi_range(-1, 1)
+	if idle:
+		new_direction = choice
+	
+func set_target_enemy():
+	var enemy_data = enemydetection_component.get_enemy()
+	enemy = enemy_data[0]
+	enemy_distance = enemy_data[1]
+
+func map_range(value: float, start1: float, stop1: float, start2: float, stop2: float):
+	return (value - start1) / (stop1 - start1) * (stop2 - start2) + start2
--- components/character_components/enemy_detection_component.gd
+++ components/character_components/enemy_detection_component.gd
@@ -80,4 +80,5 @@ func get_enemy():
 		enemy_distance = 600
 	else:
 		target_enemy = enemy_list[0]
+	print(enemy_list)
 	return [target_enemy, enemy_distance]
